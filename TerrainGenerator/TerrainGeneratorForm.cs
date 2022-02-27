c8a579716fe84e3f988b0bfea01fc7fb7f208077
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-02-27 20:46:15 +0200

    added key and mouse camera control

--- TerrainGenerator/TerrainGeneratorForm.cs
+++ TerrainGenerator/TerrainGeneratorForm.cs
@@ -19,13 +19,20 @@ namespace TerrainGenerator
         private IndexBuffer indexBuffer = null;
 
         // Size of the terain
-        private static int terrainWidth = 5;
-        private static int terrainLenght = 5;
+        private static int terrainWidth = 25;
+        private static int terrainLenght = 25;
 
         private static int verticesCount = terrainLenght * terrainLenght;
         private static int indexCount = (terrainWidth - 1) * (terrainLenght - 1) * 6;
 
         private Vector3 cameraPosition, cameraLookAt, cameraUp;
+
+        float cameraMoveSpeed = 0.2f;
+        float cameraTurnSpeed = 0.005f;
+        float rotateY = 0;
+        float tempY = 0;
+
+        bool isMiddleMouseDown = false;
 
         private CustomVertex.PositionColored[] vertices = null;
 
@@ -40,6 +47,8 @@ namespace TerrainGenerator
             InitializeComponent();
 
             InitializeGraphics();
+
+            InitializeEventHandlers();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -63,6 +72,19 @@ namespace TerrainGenerator
             this.Invalidate();
         }
 
+        private void InitializeEventHandlers()
+        {
+            vertexBuffer.Created += new EventHandler(this.OnVertexBufferCreate);
+            indexBuffer.Created += new EventHandler(this.OnIndexBufferCreate);
+
+            this.KeyDown += new KeyEventHandler(OnKeyDown);
+            this.MouseWheel += new MouseEventHandler(OnMouseScroll);
+
+            this.MouseDown += new MouseEventHandler(OnMouseDown);
+            this.MouseUp += new MouseEventHandler(OnMouseUp);
+            this.MouseMove += new MouseEventHandler(OnMouseMove);
+        }
+
         private void InitializeGraphics()
         {
             PresentParameters pp = new PresentParameters();
@@ -81,17 +103,14 @@ namespace TerrainGenerator
 
             // Initialize the veritices buffer
             vertexBuffer = new VertexBuffer(typeof(CustomVertex.PositionColored), verticesCount, device, Usage.Dynamic | Usage.WriteOnly, CustomVertex.PositionColored.Format, Pool.Default);
-            vertexBuffer.Created += new EventHandler(this.OnVertexBufferCreate);
             OnVertexBufferCreate(vertexBuffer, null);
 
             // Initialize the index buffer
             indexBuffer = new IndexBuffer(typeof(int), indexCount, device, Usage.WriteOnly, Pool.Default);
-            indexBuffer.Created += new EventHandler(this.OnIndexBufferCreate);
             OnIndexBufferCreate(indexBuffer, null);
 
             // Set initial camera position
             cameraPosition = new Vector3(2, 4.5f, -3.5f);
-            cameraLookAt = new Vector3(2, 3.5f, -2.5f);
             cameraUp = new Vector3(0, 1, 0);
         }
 
@@ -109,6 +128,10 @@ namespace TerrainGenerator
 
         private void SetupCamera()
         {
+            cameraLookAt.X = (float) Math.Sin(rotateY) + cameraPosition.X;
+            cameraLookAt.Y = cameraPosition.Y - 1;
+            cameraLookAt.Z = (float)Math.Cos(rotateY) + cameraPosition.Z;
+
             device.Transform.Projection = Matrix.PerspectiveFovLH((float)Math.PI / 4, this.Width / this.Height, 1.0f, 100.0f);
             device.Transform.View = Matrix.LookAtLH(cameraPosition, cameraLookAt, cameraUp);
 
@@ -167,6 +190,67 @@ namespace TerrainGenerator
                 }
             }
 
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    cameraPosition.X += cameraMoveSpeed * (float) Math.Sin(rotateY);
+                    cameraPosition.Z += cameraMoveSpeed * (float)Math.Cos(rotateY);
+                    break;
+                case Keys.D:
+                    cameraPosition.X += cameraMoveSpeed * (float)Math.Sin(rotateY + Math.PI / 2);
+                    cameraPosition.Z += cameraMoveSpeed * (float)Math.Cos(rotateY + Math.PI / 2);
+                    break;
+                case Keys.A:
+                    cameraPosition.X -= cameraMoveSpeed * (float)Math.Sin(rotateY + Math.PI / 2);
+                    cameraPosition.Z -= cameraMoveSpeed * (float)Math.Cos(rotateY + Math.PI / 2);
+                    break;
+                case Keys.S:
+                    cameraPosition.X -= cameraMoveSpeed * (float)Math.Sin(rotateY);
+                    cameraPosition.Z -= cameraMoveSpeed * (float)Math.Cos(rotateY);
+                    break;
+                case Keys.Q:
+                    rotateY -= cameraTurnSpeed;
+                    break;
+                case Keys.E:
+                    rotateY += cameraTurnSpeed;
+                    break;
+            }
+        }
+
+        private void OnMouseScroll(object sender, MouseEventArgs e)
+        {
+            cameraPosition.Y -= e.Delta * 0.001f;
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Middle:
+                    tempY = rotateY - e.X * cameraTurnSpeed;
+                    isMiddleMouseDown = true;
+                    break;
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Middle:
+                    isMiddleMouseDown = false;
+                    break;
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if(isMiddleMouseDown)
+                rotateY = tempY + e.X * cameraTurnSpeed;
         }
     }
 }
