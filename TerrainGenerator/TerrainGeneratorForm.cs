375ccf7ba8f5aba80d3af48027a0ee71b9923085
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-02-27 18:50:26 +0200

    made a 3d object (cube) out of triangles

--- TerrainGenerator/TerrainGeneratorForm.cs
+++ TerrainGenerator/TerrainGeneratorForm.cs
@@ -12,13 +12,26 @@ namespace TerrainGenerator
 {
     public partial class TerrainGeneratorForm : Form
     {
+        private float angle = 0.0f;
+
         private Device device = null;
 
         private VertexBuffer vertexBuffer = null;
+        
+        private IndexBuffer indexBuffer = null;
 
         private CustomVertex.PositionColored[] vertices = null;
 
-        private float angle = 0.0f;
+        // Indices for the cube corners coordinates
+        private static readonly short[] indices =
+        {
+            0, 1, 2,  1, 3, 2,   // frontside
+            4, 5, 6,  6, 5, 7,   // backside
+            0, 5, 4,  0, 2, 5,   // upside
+            1, 6, 7,  1, 7, 3,   // bottom
+            0, 6, 1,  4, 6, 0,   // left side
+            2, 3, 7,  5, 2, 7    // right side
+        };
 
 
         public TerrainGeneratorForm()
@@ -32,7 +45,7 @@ namespace TerrainGenerator
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            device.Clear(ClearFlags.Target, Color.CornflowerBlue, 1, 0);
+            device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.CornflowerBlue, 1, 0);
 
             SetupCamera();
 
@@ -40,7 +53,12 @@ namespace TerrainGenerator
 
             device.VertexFormat = CustomVertex.PositionColored.Format;
             device.SetStreamSource(0, vertexBuffer, 0);
-            device.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
+            device.Indices = indexBuffer;
+
+            DrawBox(angle/(float) Math.PI, angle / (float)Math.PI, angle / (float)Math.PI, 0, 0, 0);
+            DrawBox(angle / (float)Math.PI / 2, angle / (float)Math.PI * 3, angle / (float)Math.PI / 3*2, -0.5f, 0, 0);
+
+            angle += 0.04f;
 
             device.EndScene();
 
@@ -56,54 +74,63 @@ namespace TerrainGenerator
             pp.Windowed = true;
             pp.SwapEffect = SwapEffect.Discard;
 
+            pp.EnableAutoDepthStencil = true;
+            pp.AutoDepthStencilFormat = DepthFormat.D16;
+
             device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, pp);
 
-            vertexBuffer = new VertexBuffer(typeof(CustomVertex.PositionColored), 6, device, Usage.Dynamic | Usage.WriteOnly, CustomVertex.PositionColored.Format, Pool.Default);
+            // Initialize the veritices buffer
+            vertexBuffer = new VertexBuffer(typeof(CustomVertex.PositionColored), 8, device, Usage.Dynamic | Usage.WriteOnly, CustomVertex.PositionColored.Format, Pool.Default);
             vertexBuffer.Created += new EventHandler(this.OnVertexBufferCreate);
             OnVertexBufferCreate(vertexBuffer, null);
 
             vertexBuffer.SetData(vertices, 0, LockFlags.None);
+
+            // Initialize the index buffer
+            indexBuffer = new IndexBuffer(typeof(short), indices.Length, device, Usage.WriteOnly, Pool.Default);
+            indexBuffer.Created += new EventHandler(this.OnIndexBufferCreate);
+            OnIndexBufferCreate(indexBuffer, null);
+        }
+
+        private void OnIndexBufferCreate(object sender, EventArgs e)
+        {
+            IndexBuffer buffer = (IndexBuffer)sender;
+
+            buffer.SetData(indices, 0, LockFlags.None);
         }
 
         private void OnVertexBufferCreate(object sender, EventArgs e)
         {
-            VertexBuffer butter = (VertexBuffer)sender;
+            VertexBuffer buffer = (VertexBuffer)sender;
 
-            vertices = new CustomVertex.PositionColored[6];
+            vertices = new CustomVertex.PositionColored[8];
 
-            vertices[0] = new CustomVertex.PositionColored(-1, 1, 1, Color.Blue.ToArgb());
+            vertices[0] = new CustomVertex.PositionColored(-1, 1, 1, Color.Red.ToArgb());
             vertices[1] = new CustomVertex.PositionColored(-1, -1, 1, Color.Blue.ToArgb());
-            vertices[2] = new CustomVertex.PositionColored(1, 1, 1, Color.Blue.ToArgb());
+            vertices[2] = new CustomVertex.PositionColored(1, 1, 1, Color.Green.ToArgb());
+            vertices[3] = new CustomVertex.PositionColored(1, -1, 1, Color.Gold.ToArgb());
+            vertices[4] = new CustomVertex.PositionColored(-1, 1, -1, Color.Gold.ToArgb());
+            vertices[5] = new CustomVertex.PositionColored(1, 1, -1, Color.Green.ToArgb());
+            vertices[6] = new CustomVertex.PositionColored(-1, -1, -1, Color.Blue.ToArgb());
+            vertices[7] = new CustomVertex.PositionColored(1, -1, -1, Color.Red.ToArgb());
 
-            vertices[3] = new CustomVertex.PositionColored(-1, -1, 1, Color.Red.ToArgb());
-            vertices[4] = new CustomVertex.PositionColored(1, -1, 1, Color.Red.ToArgb());
-            vertices[5] = new CustomVertex.PositionColored(1, 1, 1, Color.Red.ToArgb());
-
-
-            vertices[0] = new CustomVertex.PositionColored(-1, 1, -1, Color.Blue.ToArgb());
-            vertices[1] = new CustomVertex.PositionColored(-1, -1, -1, Color.Blue.ToArgb());
-            vertices[2] = new CustomVertex.PositionColored(1, 1, -1, Color.Blue.ToArgb());
-
-            vertices[3] = new CustomVertex.PositionColored(-1, -1, -1, Color.Red.ToArgb());
-            vertices[4] = new CustomVertex.PositionColored(1, -1, -1, Color.Red.ToArgb());
-            vertices[5] = new CustomVertex.PositionColored(1, 1, -1, Color.Red.ToArgb());
-
-
-
-
-            butter.SetData(vertices, 0, LockFlags.None);
+            buffer.SetData(vertices, 0, LockFlags.None);
         }
 
         private void SetupCamera()
         {
             device.Transform.Projection = Matrix.PerspectiveFovLH((float)Math.PI / 4, this.Width / this.Height, 1.0f, 100.0f);
-            device.Transform.View = Matrix.LookAtLH(new Vector3(0, 0, -5), new Vector3(), new Vector3(0, 1, 0));
-
-            device.Transform.World = Matrix.RotationX(angle);
-            angle += 0.05f;
+            device.Transform.View = Matrix.LookAtLH(new Vector3(0, 0, 10), new Vector3(), new Vector3(0, 1, 0));
 
             device.RenderState.Lighting = false;
             device.RenderState.CullMode = Cull.None;
+        }
+
+        private void DrawBox(float yaw, float pitch, float roll, float x, float y, float z)
+        {
+            device.Transform.World = Matrix.RotationYawPitchRoll(yaw, pitch, roll) * (Matrix.Translation(x, y, z));
+            
+            device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 8, 0, indices.Length/3);
         }
     }
 }
